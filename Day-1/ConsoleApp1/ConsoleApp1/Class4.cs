e8111f082dd9442f150e45f922a26372bee38c06
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-15 16:24:41 +0530

    first commit

--- Day-1/ConsoleApp1/ConsoleApp1/Class4.cs
+++ Day-1/ConsoleApp1/ConsoleApp1/Class4.cs
@@ -7,6 +7,14 @@ namespace ConsoleApp1
     internal class Class4
     {
         //Switch
+        //iteration statements
+        //looping statements
+        /*
+         * 1.for loop
+         * 2.while loop
+         * 3.do while
+         * 4.for each
+         */
         public static void Main()
         {
             int choice=3;
@@ -22,6 +30,55 @@ namespace ConsoleApp1
                     Console.WriteLine("default block is called");
                     break;
             }
+
+            /*
+             * for loop 
+             * for(<intialization>;expression;increment/decrement){
+             * 
+             * 
+             */
+
+            //Console.Write("\n");  === Console.WriteLine();
+            Console.WriteLine("====================For Loop===================");
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    Console.Write(" row= "+row + " col= "+col);
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+            /*
+             * while loop Entry control loop
+             * <intialization>;
+             * while(expression){
+             * increment/decrement;
+             * }
+             * 
+             * do while loop  exit control loop
+             * <intialization>;
+             * do{
+             * increment/decrement;
+             * }while(expression);
+             * 
+             */
+            Console.WriteLine("====================while Loop===================");
+            int j = 0;
+            while (j < 5)
+            {
+                Console.Write(j + "\t");
+                j++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("====================do while Loop===================");
+            int k = 0;
+            do
+            {
+                Console.Write(k + "\t");
+                k++;
+            } while (k <5);
+
         }
     }
 }
