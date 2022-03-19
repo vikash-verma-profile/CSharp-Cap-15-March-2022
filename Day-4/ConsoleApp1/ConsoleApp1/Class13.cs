4a44a2bc48f95a3697993d3dd21fede75a9dc1af
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-19 17:17:13 +0530

    first commit

--- Day-4/ConsoleApp1/ConsoleApp1/Class13.cs
+++ Day-4/ConsoleApp1/ConsoleApp1/Class13.cs
@@ -8,13 +8,13 @@ namespace ConsoleApp1
     {
         public enum shapes
         {
-            circle,
-            square
+            circle=1,
+            square=2
         }
 
         public void peri(int val ,shapes s1)
         {
-            if (s1 == 0)
+            if ((int)s1 == 1)
             {
                 Console.WriteLine(2*3.14*val);
             }
@@ -25,7 +25,7 @@ namespace ConsoleApp1
     }
     internal class Class13
     {
-        public static void Main()
+        public static void Main14()
         {
             Parameter a1=new Parameter();
             a1.peri(3,Parameter.shapes.circle);
---
+++ Day-4/ConsoleApp1/ConsoleApp1/Class14.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /*
+     * array
+     * Structure==> hetrogeneous type of data struct
+     * 
+     * Fields
+     * Parameterized comstructor
+     * Constants
+     * Properties
+     * Methods
+     * 
+     */
+
+    struct Student
+    {
+        public string Name;
+        public int Age;
+        public int Roll_no;
+    }
+    internal class Class14
+    {
+        public static void Main()
+        {
+            Student student;
+
+            student.Name = "Vikash";
+            student.Age = 30;
+            student.Roll_no = 101;
+
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Age);
+            Console.WriteLine(student.Roll_no);
+
+        }
+    }
+}
