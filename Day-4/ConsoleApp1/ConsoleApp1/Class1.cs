9efbb73bd57345e1299fe772e7ade1afc3ff7a1e
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-19 09:31:59 +0530

    first commit

---
+++ Day-4/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class ClassA
+    {
+        int n1, n2;
+        public ClassA()
+        {
+            Console.WriteLine("Base class Constructor is called");
+        }
+        public ClassA(int i,int j)
+        {
+            n1 = i;
+            n2 = j;
+            Console.WriteLine("Values are n1= "+n1+" n2= "+n2);
+        }
+    }
+    public class DerivedClass : ClassA
+    {
+        public DerivedClass():base()
+        {
+
+        }
+        public DerivedClass(int i,int j):base(i,j)
+        {
+
+        }
+    }
+    /*
+     * base keyword while calling base-class constructor from dervied class 
+     * when derived class instnaces are created
+     */
+    internal class Class1
+    {
+        public static void Main()
+        {
+            DerivedClass d1 = new DerivedClass();
+            Console.WriteLine();
+            DerivedClass d2 = new DerivedClass(10,20);
+        }
+    }
+}
---
+++ Day-4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class web
+    {
+        string name = "Base Keyword";
+        public virtual void Show()
+        {
+            Console.WriteLine(name);    
+        }
+    }
+    class Stream : web
+    {
+        string subject = "C sharp";
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine(subject);
+        }
+    }
+    internal class Program
+    {
+
+        /*base keyword
+        1.if you want to call methods or function from derived class
+        2.call constructors internally of base class at the time of inheritence
+        */
+        static void Main1(string[] args)
+        {
+            Stream stream = new Stream();
+            stream.Show();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
