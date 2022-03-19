cbc61a76f56af82ba997ffc0497d8cfb3be052a1
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-19 11:33:45 +0530

    first commit

--- Day-4/ConsoleApp1/ConsoleApp1/Class2.cs
+++ Day-4/ConsoleApp1/ConsoleApp1/Class2.cs
@@ -7,9 +7,19 @@ namespace ConsoleApp1
 	public class Person
 	{
 		public string name;
+
+        public Person(string _name)
+        {
+			name = _name;
+        }
 		public override string ToString()
 		{
 			return "Hello my name is " + name;
+		}
+        ~ Person()
+        {
+			name = string.Empty;
+
 		}
 	}
 	internal class Class2
@@ -22,8 +32,8 @@ namespace ConsoleApp1
 			{
 				Console.WriteLine("Enter name:");
 
-				p1[i] = new Person { name = Console.ReadLine() };
-
+				//p1[i] = new Person { name = Console.ReadLine() };
+				p1[i] = new Person(Console.ReadLine());
 				//Person p=new Person();
 				//p.name=Console.ReadLine();
 				//p1[i] = p;
---
+++ Day-4/ConsoleApp1/ConsoleApp1/Class3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Class3
+    {
+        public static void Main3()
+        {
+
+        }
+    }
+}
